7ee43f15843706aa646d8f3c925c1707c3d79242
Andrii <scoandrey@gmail.com>
2023-04-03 23:04:23 -0400

    n2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
 
+
+
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+
+    const onPlay = function(data) {
+        // data is an object containing properties specific to that event
+    };
+    
+    player.on('play', onPlay);
+
+
+    
+    player.on('eventName', function(data) {
+        // data is an object containing properties specific to that event
+    });
+
+
+    let callback = function() {};
+
+player.off('eventName', callback);
