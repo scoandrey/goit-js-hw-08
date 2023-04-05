94877a07a474be49f69418d6649b662e6699f598
scoandrey <32551449+scoandrey@users.noreply.github.com>
2023-04-05 05:19:19 +0000

    Deploying to gh-pages from @ scoandrey/goit-js-hw-08@d3ef1c15f9c93cd1060f45025ffa833282db1fae 🚀

--- 02-video.51c4cd7e.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),n=new Vimeo.Player(e);n.on("play",(function(){console.log("played the video!")})),n.getVideoTitle().then((function(e){console.log("title:",e)}));n.on("play",(function(e){})),n.on("eventName",(function(e){}));n.off("eventName",(function(){}));
-//# sourceMappingURL=02-video.51c4cd7e.js.map
