ea095074989f4c26f5b3c58069fce4ff732de300
Andrii <scoandrey@gmail.com>
2023-04-05 10:07:25 -0400

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,24 +1,10 @@
 
-import throttle from 'lodash.throttle';
+    import throttle from 'lodash.throttle';
 import Player  from '@vimeo/player';
-
-
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-
-
-player.on('play', function() {
-    console.log('played the video!');
-});
-
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
 const onPlay = function(time) {
-    localStorage.setItem("videoplayer-current-time", JSON.stringify(time)).then(function(seconds){
-    localStorage.getItem("videoplayer-current-time")}).catch(function(error) {
-        console.log('mistake')
-    });
-
+    localStorage.setItem('videoplayer-current-time', `${time.seconds}`);
 };
     player.on('timeupdate', throttle((onPlay), 1000));
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
