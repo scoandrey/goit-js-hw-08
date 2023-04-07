8099500737c55393371f08e6fb15bed8be8c0aae
Andrii <scoandrey@gmail.com>
2023-04-06 22:04:13 -0400

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,20 +1,25 @@
-// import throttle from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 
 const refs = {
 form: document.querySelector('.feedback-form'),
 textarea: document.querySelector('.textarea'),
 emailInput: document.querySelector('.emailInput'),
 };
+const formData = {};
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', onTextareaInput);
-refs.emailInput.addEventListener('input', onEmailInput);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+refs.emailInput.addEventListener('input', throttle(onEmailInput, 500));
+refs.form.addEventListener('input', e => {
+formData[e.target.name] = e.target.value;
+})
 
 function onFormSubmit(e) {
 e.preventDefault();
 e.target.reset();
 localStorage.removeItem("feedback-form-state");
 localStorage.removeItem("feedback-form");
+console.log(formData);
 };
 
 function onTextareaInput(e) {   
@@ -41,3 +46,4 @@ refs.emailInput.value = savedText;
 
 outputText();
 outputEmail();
+
