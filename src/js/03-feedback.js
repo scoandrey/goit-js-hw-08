827f3e210c76bf2e00098c2f487aaf50d0dacaa2
Andrii <scoandrey@gmail.com>
2023-04-09 17:27:23 -0400

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,12 +4,11 @@ const STORAGE_KEY = "feedback-form-state";
 const refs = {
 form: document.querySelector('.feedback-form'),
 // textarea: document.querySelector('.textarea'),
-// emailInput: document.querySelector('.emailInput'),
+
 };
 populateTextarea();
 refs.form.addEventListener('submit', onFormSubmit);
 // refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
-// refs.emailInput.addEventListener('input', throttle(onEmailInput, 500));
 // refs.textarea.addEventListener('input', onTextareaInput);
 refs.form.addEventListener('input', throttle(onInput, 500));
 
@@ -30,7 +29,7 @@ function populateTextarea() {
     const savedText = localStorage.getItem(STORAGE_KEY);
     if (savedText){
     refs.form.value = savedText;   
-    // console.log(savedText);
+    
     }
 }
 // populateTextarea()
@@ -39,23 +38,5 @@ function populateTextarea() {
 // localStorage.setItem(STORAGE_KEY, message);
 // };
 
-// function outputText() {
-// const savedText = localStorage.getItem("feedback-form-state");
-// if(savedText) {
-// refs.textarea.value = savedText;
-// }};
 
-// function onEmailInput(e) {
-// const email = e.target.value;
-// localStorage.setItem("feedback-form", email)
-// };
-
-// function outputEmail() {
-// const savedText = localStorage.getItem("feedback-form");
-// if(savedText) {
-// refs.emailInput.value = savedText;
-// }};
-
-// outputText();
-// outputEmail();
 
