15be10c6cbf46c506d2a3ca214439269067068c2
Andrii <scoandrey@gmail.com>
2023-04-10 14:13:42 -0400

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,31 +4,25 @@ const form = document.querySelector('.feedback-form');
 const emailInput = document.querySelector('.text');
 const messageInput = document.querySelector('.email');
 const STORAGE_KEY = "feedback-form-state";
-const savedForm = throttle(() => {
 const formData = {
-    email: emailInput.value,
-    message: messageInput.value,
-  };
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-}, 500);
+  email: emailInput.value,
+  message: messageInput.value,
+};
+const savedForm = throttle(localStorage.setItem(STORAGE_KEY, JSON.stringify(formData)), 500);
 
 emailInput.addEventListener('input', savedForm);
 messageInput.addEventListener('input', savedForm);
 
-const fillFormFromStorage = () => {
-  const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
-  emailInput.value = formData.email || '';
-  messageInput.value = formData.message || '';
+const fillFormFromStorage = function () {
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  emailInput.value = formData.email;
+  messageInput.value = formData.message;
 };
 
 fillFormFromStorage();
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const formData = {
-    email: emailInput.value,
-    message: messageInput.value,
-  };
   console.log(formData);
   localStorage.removeItem(STORAGE_KEY);
   e.target.reset();
